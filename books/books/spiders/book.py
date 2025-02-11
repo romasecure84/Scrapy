635eb783afb696a8f0fb8588a79f8a2041f9fffa
https://github.com/romasecure84/BaseOfSelenium.git <romasecure84@yanex.ru>
2025-02-11 22:37:49 +0400

    Add BookSpider skeleton for scraping books
    Introduce a basic BookSpider class with necessary attributes like `name`, `allowed_domains`, and `start_urls`. Includes a placeholder `parse` method for future implementation.

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -7,4 +7,13 @@ class BookSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
+        books_links = response.css("article.product_pod h3 a::attr(href)").getall()
+        for book_link in books_links:
+            yield response.follow(book_link, callback=self.parse_book)
+
+        next_page_link = response.css("li.next a::attr(href)").get()
+        if next_page_link is not None:
+            yield response.follow(next_page_link, callback=self.parse)
+
+    def parse_book(self, response):
         pass
