d8c465b2cf9f556386cce5d563318d053463b713
https://github.com/romasecure84/BaseOfSelenium.git <romasecure84@yanex.ru>
2025-02-11 23:15:27 +0400

    Add book parsing logic to BookSpider
    Implement detailed parsing of book data, including name, price, UPC, availability, tax, category, star rating, and image URL. Introduce a `number_dict` to map star ratings from words to numbers and utilize `BookItem` for structured data storage.

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -1,4 +1,7 @@
 import scrapy
+from ..items import BookItem
+
+number_dict = {'One': '1', 'Two': '2', 'Three': '3', 'Four': '4', 'Five': '5'}
 
 
 class BookSpider(scrapy.Spider):
@@ -16,4 +19,24 @@ class BookSpider(scrapy.Spider):
             yield response.follow(next_page_link, callback=self.parse)
 
     def parse_book(self, response):
-        pass
+        book = BookItem()
+
+        book["name"] = response.css("div.product_main h1::text").get()
+
+        book["price_excl_tax"] = response.xpath('//th[text()="Price (excl. tax)"]/following-sibling::td[1]/text()').get()
+        book["price_incl_tax"] = response.xpath('//th[text()="Price (incl. tax)"]/following-sibling::td[1]/text()').get()
+        book["upc"] = response.xpath('//th[text()="UPC"]/following-sibling::td[1]/text()').get()
+        book["availability"] = response.xpath('//th[text()="Availability"]/following-sibling::td[1]/text()').get()
+        book["tax"] = response.xpath('//th[text()="Tax"]/following-sibling::td[1]/text()').get()
+
+        category_children = response.xpath('//ul[@class="breadcrumb"]/child::*')
+        book["category"] = category_children[2].css("a::text").get()
+
+        star_tag = response.css('p.star-rating')
+        class_name_string = star_tag.attrib["class"]
+        stars = class_name_string.split(" ")[-1]
+        book["stars"] = number_dict[stars]
+
+        book["image_url"] = 'https://books.toscrape.com' + response.css("div.active img").attrib["src"][5:]
+
+        yield book
